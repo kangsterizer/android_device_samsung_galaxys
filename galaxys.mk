f11f36b8d054d690923871f874140994c8ec6568
Kolja Dummann <k.dummann@gmail.com>
2011-03-06 09:19:48 +0100

    fix typo

--- galaxys.mk
+++ galaxys.mk
@@ -173,9 +173,9 @@ out/target/product/galaxys/kernel_build/.config:
 	$(hide) mkdir -p $(PRODUCT_OUT)/kernel_build
 	$(hide) $(MAKE) -C kernel/samsung/2.6.35 O=$(ANDROID_BUILD_TOP)/$(PRODUCT_OUT)/kernel_build aries_galaxys_defconfig
 
-out/target/product/captivate/kernel: out/target/product/galaxys/recovery.img out/target/product/galaxys/kernel_build/.config build_kernel
-  @echo "BUILDING KERNEL"
-  @echo "recovery.img size: `ls -l out/target/product/galaxys/recovery.img`"
+out/target/product/galaxys/kernel: out/target/product/galaxys/recovery.img out/target/product/galaxys/kernel_build/.config build_kernel
+	@echo "BUILDING KERNEL"
+	@echo "recovery.img size: `ls -l out/target/product/galaxys/recovery.img`"
 	$(hide) $(MAKE) -C kernel/samsung/2.6.35 O=$(ANDROID_BUILD_TOP)/$(PRODUCT_OUT)/kernel_build CROSS_COMPILE=$(ANDROID_BUILD_TOP)/$(subst -gcc,-,$(TARGET_CC))
 	$(hide) $(ACP) $(PRODUCT_OUT)/kernel_build/arch/arm/boot/zImage $(PRODUCT_OUT)/kernel
 
