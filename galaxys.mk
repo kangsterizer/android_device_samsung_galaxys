3f846327e487476298df3d1a5e97a2c54f2dbe95
Daniel Hillenbrand <info@codeworkx.de>
2011-03-11 03:26:42 -0800

    galaxys.mk: disable extended JNI checks

--- galaxys.mk
+++ galaxys.mk
@@ -139,6 +139,14 @@ PRODUCT_PROPERTY_OVERRIDES += \
         ro.com.google.locationfeatures=1 \
         ro.com.google.networklocation=1
 
+# Extended JNI checks
+# The extended JNI checks will cause the system to run more slowly, but they can spot a variety of nasty bugs 
+# before they have a chance to cause problems.
+# Default=true for development builds, set by android buildsystem.
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.kernel.android.checkjni=0 \
+    dalvik.vm.checkjni=false
+
 # we have enough storage space to hold precise GC data
 PRODUCT_TAGS += dalvik.gc.type-precise
 
@@ -175,8 +183,6 @@ out/target/product/galaxys/kernel: out/target/product/galaxys/recovery.img out/t
 	@echo "recovery.img size: `ls -l out/target/product/galaxys/recovery.img`"
 	$(hide) $(MAKE) -C kernel/samsung/2.6.35 O=$(ANDROID_BUILD_TOP)/$(PRODUCT_OUT)/kernel_build CROSS_COMPILE=$(ANDROID_BUILD_TOP)/$(subst -gcc,-,$(TARGET_CC))
 	$(hide) $(ACP) $(PRODUCT_OUT)/kernel_build/arch/arm/boot/zImage $(PRODUCT_OUT)/kernel
-
-
 
 # See comment at the top of this file. This is where the other
 # half of the device-specific product definition file takes care
